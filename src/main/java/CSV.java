d6aab8ba5d8e3ef669ba5c12672d66125d0d059d
Benyamin Galeano <benyamin.galeano@pm.me>
2020-06-02 21:32:09 -0600

    mejoras

--- src/main/java/CSV.java
+++ src/main/java/CSV.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.TreeMap;
 
@@ -9,29 +12,56 @@ import display.Display;
 import config.ConfigParser;
 
 public class CSV {
+    private static final SimpleDateFormat INPUTDATEFORMAT = new SimpleDateFormat("yyyyMMdd");
+    private static final SimpleDateFormat GOOGLEDATEFORMAT = new SimpleDateFormat("yyyy/MM/dd");
+    private static Date endDate = null;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        
+        if (args.length == 1) {
+            endDate = INPUTDATEFORMAT.parse(args[0]);
+        }
+
+        final File inputDir = new File("input");
+        final File outputDir = new File("output");
+
+        if (!inputDir.exists()) {
+            inputDir.mkdir();
+        }
+
+        if (!outputDir.exists()) {
+            outputDir.mkdir();
+        }
 
         /* Observador para archivos de entrada */
         CSVWatcher wIn = new CSVWatcher(
-            new ConfigParser("input.config"), 
-            new File("./input").listFiles((file, fname) -> fname.matches(".*\\.csv"))
+            new ConfigParser("input.yaml"), 
+            inputDir.listFiles((file, fname) -> fname.matches(".*\\.csv"))
         );
 
         /* Observador para archivos de salida */
         CSVWatcher wOut = new CSVWatcher(
-            new ConfigParser("output.config"), 
-            new File("./output").listFiles((file, fname) -> fname.matches(".*\\.csv"))
+            new ConfigParser("output.yaml"), 
+            outputDir.listFiles((file, fname) -> fname.matches(".*\\.csv"))
         );
 
+        System.out.println(wOut.settings);
+
         /* Texto con formato y titulos personalizados */
-        Display display = new Display(new ConfigParser("display.config"));
+        Display display = new Display(new ConfigParser("display.yaml"));
 
         TreeMap<String,Double> ids = new TreeMap<>();
 
         final int idColIn = wIn.settings.get("idCol", Integer.class) - 1;
         final int gradeColIn = wIn.settings.get("gradeCol", Integer.class) - 1;
-        wIn.consumeCSVs((row)->{
+        wIn.consumeCSVs((row) ->{
+            if (endDate != null) {
+                try {
+                    if (GOOGLEDATEFORMAT.parse(row.get(0)).after(endDate)){
+                        return;
+                    }
+                } catch (ParseException e) {}
+            }
             final String[] aux = row.get(gradeColIn).replaceAll("\"", "").split("/");
             final double gradeAux = Math.round(Double.parseDouble(aux[0].trim()) / Double.parseDouble(aux[1]) * wOut.settings.get("grade", Integer.class));
             
@@ -50,7 +80,7 @@ public class CSV {
                 private static final long serialVersionUID = 1L;
                 {
                     put("gradeCol", value.toString());
-                    put("commentCol", wOut.settings.get("comment", String.class));
+                    put("commentCol", wOut.settings.get("comment", String.class) == null ? "" : wOut.settings.get("comment", String.class));
                 }
             }, "idCol", key, "gradeCol");
             if(result != null) return;
--- src/main/java/config/ConfigParser.java
+++ src/main/java/config/ConfigParser.java
@@ -1,15 +1,88 @@
 package config;
 
 import java.io.File;
+import java.io.FileInputStream;
+
+import org.yaml.snakeyaml.*;
 import java.io.FileNotFoundException;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.TreeMap;
+
+public class ConfigParser {
+    private final LinkedHashMap<String, Object> settings = new LinkedHashMap<>();
+    private final LinkedList<String> errors = new LinkedList<>();
+
+    private void appendError(final String inf, final String current, final int line, final String exInfo){
+        errors.add(inf+"\n>> "+current+"\nLinea "+line+" "+exInfo);
+    }
+
+    private void appendError(final String inf, final String current, final int line){
+        appendError(inf, current, line, "");
+    }
+
+    public ConfigParser(final String configPath)  {
+        final File file = new File(configPath);
+        Yaml parser = new Yaml();
+        try {
+            this.settings.putAll(parser.load(new FileInputStream(file)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public <T> T get(final String value, final Class<T> clss){
+        if(clss == String.class){
+            return clss.cast(settings.get(value));
+        } else if(clss == Double.class){
+            return clss.cast(settings.get(value));
+        } else if(clss == Integer.class){
+            return clss.cast(settings.get(value));
+        }
+        return null;
+    }
+
+    public LinkedList<Integer> getAllCols(){
+        LinkedList<Integer> buff = new LinkedList<>();
+
+        settings
+        .keySet()
+        .stream()
+        .filter((element)->element.contains("Col"))
+        .map((key)->settings.get(key))
+        .map((element) -> {
+            try {
+                return (int) element;
+            } catch (Exception e) {
+               return null;
+            }
+        })
+        .filter((element)->element!=null)
+        .forEach(element->buff.add(element));
+
+        return buff;
+    }
+
+    public String errors(){
+        return errors.stream().reduce((er1,er2)->er1.concat("\n").concat(er2)).orElse("");
+    }
+
+    public boolean hasErrors(){
+        return errors.size() > 0;
+    }
+
+    @Override
+    public String toString() {
+        return settings.toString();
+    }
+}
 
 /**
  * ConfigParser
  */
-public class ConfigParser {
+/* public class ConfigParser {
     private final TreeMap<String, String> settings = new TreeMap<>();
     private final LinkedList<String> errors = new LinkedList<>();
 
@@ -132,4 +205,4 @@ public class ConfigParser {
     public String toString() {
         return settings.toString();
     }
-}
+} */
